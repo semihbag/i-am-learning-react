78983dc1bc76668b6725eef5352c0ab1b7546c8b
semihbag <semih702bag@gmail.com>
2023-10-29 17:56:17 +0300

    develop-semih / hello world working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello Wolrd</h1>;
+const element = <h1>Hello Wolrd!</h1>;
 ReactDOM.render(element, document.getElementById('root'));
